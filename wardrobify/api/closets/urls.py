8036e971686e1ff4fccdac079ab3c4c132d2ccf3
rbbitphoto <rbbitphoto@gmail.com>
2022-10-10 23:08:55 -0700

    added excalidraw file to test

--- wardrobify/api/closets/urls.py
+++ wardrobify/api/closets/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import closet_list, closet_details
 
 urlpatterns = [
-    path("all/", closet_list, name="closets_list")
+    path("", closet_list, name="closets_list"),
+    path("<int:pk>/", closet_details, name="closet_details"),
 ]
--- wardrobify/api/wardrobify_project/settings.py
+++ wardrobify/api/wardrobify_project/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import dj_database_url
+import dj_database_url #noqa
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
