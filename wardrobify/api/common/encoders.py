a24b56ed1937aa805cee6e67f308ebe101e52287
rbbitphoto <rbbitphoto@gmail.com>
2022-11-24 02:04:32 -0800

    added landing page and react router dom + type defs for react router dom

--- wardrobify/api/common/encoders.py
+++ wardrobify/api/common/encoders.py
@@ -21,7 +21,8 @@ class OutfitEncoder(ModelEncoder):
 
     def get_extra_data(self, o):
         return {
-            "closet": o.closet.name
+            "closet": o.closet.name,
+            "closet_number": o.closet.closet_number
         }
 
 class ClothingItemEncoder(ModelEncoder):
@@ -35,59 +36,3 @@ class ClothingItemEncoder(ModelEncoder):
             "category": o.category.name,
         }
 
-
-# class TopEncoder(ModelEncoder):
-#     model = Top
-#     properties = ["id", "name", "brand", "top_type"]
-
-
-# class BottomEncoder(ModelEncoder):
-#     model = Bottom
-#     properties = ["id", "name", "brand", "bottom_type"]
-
-
-# class UndergarmentEncoder(ModelEncoder):
-#     model = Undergarment
-#     properties = ["id", "name", "brand", "undergarment_type"]
-
-
-# class AccessoriesEncoder(ModelEncoder):
-#     model = Accessorie
-#     properties = ["id", "name", "brand", "accessory_type"]
-
-
-# class FootwearEncoder(ModelEncoder):
-#     model = Footwear
-#     properties = ["id", "name", "brand", "footwear_type"]
-
-
-# class ClosetsEncoder(ModelEncoder):
-#     model = Closet
-#     properties = ["id", "name", "closet_number", "closet_size"]
-
-
-# class OutfitsEncoder(ModelEncoder):
-#     model = Outfit
-#     properties = [
-#         "id",
-#         "outfit_name",
-#         "accessories",
-#         "bottom",
-#         "footwear",
-#         "top",
-#         "undergarment"
-#     ]
-
-#     def get_extra_data(self, o):
-#         return {
-#             "accessories": o.accessories.name,
-
-#         }
-
-# encoders = {
-#     "accessories": AccessoriesEncoder(),
-#     "bottom": BottomEncoder(),
-#     "footwear": FootwearEncoder(),
-#     "top": TopEncoder(),
-#     "undergarment": UndergarmentEncoder()
-# }
